4fb3903bc4a878bb5511a697c38245c994799cb3
3rdkira <159865977+3rdkira@users.noreply.github.com>
2025-02-06 22:35:36 +0000

    updating files

--- CardGame.java
+++ CardGame.java
@@ -1,4 +1,6 @@
-package cardGame;
+// Nue Lopez, Veejay Vue, Jacob Butler, ...
+// 1/28/2025
+// Card Game With OOD
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,15 +8,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CardGame {
-
+	//Initializes array...
 	private static ArrayList<Card> deckOfCards = new ArrayList<Card>();
 	private static ArrayList<Card> playerCards = new ArrayList<Card>();
-
+	//                                                                                                                                          private static ArrayList<Card> playerCards2 = new ArrayList<Card>();
 
 	public static void main(String[] args) {
+		Scanner input = null; //Input hasn't been used yet.
 
-		Scanner input = null;
-		try {
+		//Added how many cards the player wants.
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("How many cards do you want? ");
+		int numOfCards = scnr.nextInt();
+		Players player1 = new Players();
+		
+		System.out.println("How many players want to play? ");
+		int numPlayers = scnr.nextInt();
+		player1.setPlayerCount(numPlayers);
+
+		//This checks if there is an error, it will accept it even though there is an error. 
+		try {//Reads in from another text file the cards. It is so that way it doesn't take all the space in the main.
 			input = new Scanner(new File("cards.txt"));
 		} catch (FileNotFoundException e) {
 			// error
@@ -30,26 +43,26 @@ public class CardGame {
 			deckOfCards.add(newCard);	
 		}
 
-		shuffle();
-
+		shuffle(); //Shuffles the cards when called for.
 		//for(Card c: deckOfCards)
 			//System.out.println(c);
 
-		//deal the player 5 cards
-		for(int i = 0; i < 4; i++) {
+		//deal the player x amount of cards
+		for(int i = 0; i < numOfCards; i++) {
+			//playerCards.get(i).getSuit(); //Gets suit
 			playerCards.add(deckOfCards.remove(i));
 		}
 		
+		//Prints out the player's cards.
 		System.out.println("players cards");
 		for(Card c: playerCards)
 			System.out.println(c);
-
+		
+		//Line says if there are any pairs in the decks dealt.
 		System.out.println("pairs is " + checkFor2Kind());
-
 	}//end main
 
 	public static void shuffle() {
-
 		//shuffling the cards by deleting and reinserting
 		for (int i = 0; i < deckOfCards.size(); i++) {
 			int index = (int) (Math.random()*deckOfCards.size());
@@ -61,23 +74,27 @@ public class CardGame {
 
 	//check for 2 of a kind in the players hand
 	public static boolean checkFor2Kind() {
-
 		int count = 0;
 		for(int i = 0; i < playerCards.size() - 1; i++) {
 			Card current = playerCards.get(i);
 			Card next = playerCards.get(i+1);
+
+			//Going to add a three of a kind comparison...?
+			/*
+			Card third = playerCards.get(i+2);
+			*/
 			
 			for(int j = i+1; j < playerCards.size(); j++) {
 				next = playerCards.get(j);
-				//System.out.println(" comparing " + current);
-				//System.out.println(" to " + next);
+				//System.out.println(" comparing " + current); //comment out afterwards
+				//System.out.println(" to " + next); //comment out afterwards
 				if(current.equals(next))
 					count++;
 			}//end of inner for
 			if(count == 1)
 				return true;
-
 		}//end outer for
 		return false;
 	}
+	
 }//end class
